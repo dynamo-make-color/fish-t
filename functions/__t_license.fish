bc6af773a0b3fba599d42e87389a64c4ee279f8b
Trần Đức Nam <dynamo.foss@gmail.com>
2021-03-17 00:31:09 +0700

    tmp

--- functions/__t_license.fish
+++ functions/__t_license.fish
@@ -1,9 +1,13 @@
 function __t_license -d "Create license"
   set -l url https://api.github.com/licenses
+  commandline -f repaint
   if test -z $argv[1]
     curl -sSL $url | jq '.[].key' | sed -e 's/^"//' -e 's/"$//'
   else
-    set -l template (curl -sSL $url/$argv[1] | jq '.body')
-    echo -e $template | sed -e 's/^"//' -e 's/"$//' -e 's/\\n/\r/'
+    if not set -q t_licenses_list
+      set -l template (curl -sSL $url/$argv[1] | jq '.body')
+      set -gx t_licenses_list (echo -e $template | sed -e 's/^"//' -e 's/"$//' -e 's/\\n/\r/')
+    end
+    echo $t_licenses_list
   end
 end
