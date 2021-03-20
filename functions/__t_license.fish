687f2fc0e4bbf0719c50cee97d2a5176269c6686
Trần Đức Nam <dynamo.foss@gmail.com>
2021-03-20 22:36:38 +0700

    Resolved #3: Replace year and author (in git directory) for license cmd

--- functions/__t_license.fish
+++ functions/__t_license.fish
@@ -11,6 +11,16 @@ function __t_license -d "Create license"
     echo $t_licenses_list | string split " "
   else
     set -l template (curl -sSL -H $header $url/$argv[1] | jq '.body')
-    echo -e $template | string replace -ar '^"' '' | string replace -ar '"$' '' | string split "\\n" > LICENSE
+    set -l year (date +%Y)
+    echo -e $template \
+      | string replace -ar '^"' '' \
+      | string replace -ar '"$' '' \
+      | string split "\\n" \
+      | string replace -a '[year]' $year > LICENSE
+
+    if __fish_is_git_repository
+      set -l author (git config user.name)' \<'(git config user.email)'\>'
+      sed -i 's/\[fullname\]/'$author'/g' LICENSE
+    end
   end
 end
